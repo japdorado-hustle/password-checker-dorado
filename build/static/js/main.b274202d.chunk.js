53469981daca0b6ac2b62fc4f3e99103e3504809
japdorado-hustle <japdorado.hustle@gmail.com>
2021-11-23 14:45:16 +0800

    add build

--- build/static/js/main.b274202d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppassword-checker-dorado"]=this["webpackJsonppassword-checker-dorado"]||[]).push([[0],{10:function(e,c,r){"use strict";r.r(c);var t=r(1),n=r.n(t),s=r(3),o=r.n(s),a=(r(8),r(0));var d=function(){return Object(a.jsx)("div",{className:"App",children:"??ap"})};o.a.render(Object(a.jsx)(n.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))},8:function(e,c,r){}},[[10,1,2]]]);
-//# sourceMappingURL=main.b274202d.chunk.js.map
